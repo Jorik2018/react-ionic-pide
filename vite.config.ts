ccc8e6e7e885fb8fac27f43bbb5a7e98ac60db71
Jorik2018 <antoniopinedo2018@gmail.com>
2023-04-19 17:15:00 -0500

    fix base app

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig , loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-  }
-})
+export default ({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  return defineConfig({
+    plugins: [react()],
+    base:process.env.VITE_BASE,
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './src/setupTests.ts',
+    }
+  })
+}
